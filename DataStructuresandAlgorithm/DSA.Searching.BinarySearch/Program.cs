3a9547567ef3ac98a746fc471a69421cd6a41d19
SelvaMohandoss0611 <selvamohandoss@gmail.com>
2017-03-15 23:05:09 -0700

    init

---
+++ DataStructuresandAlgorithm/DSA.Searching.BinarySearch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Searching.BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Program pbj = new Program();
+            
+
+        }
+
+        public bool BinarySearch(int[] arr,int size,int value)
+        {
+            int low = 0, high = size - 1, mid;
+            while(low <= high)
+            {
+                mid = low + (high - low) / 2;
+                if (arr[mid] == value)
+                {
+                    return true;
+                }
+                else if (arr[mid] < value)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+
+            }
+            return false;
+
+
+        }
+   
+    }
+}
---
+++ DataStructuresandAlgorithm/DSA.Searching.LinearSearch/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Searching.LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] arr = { 1, 4, 7, 2, 3, 100, 34, 23, 78 };
+            Program pobj = new Program();           
+            bool linearSearchUnsortedOutput = pobj.linearSearchUnsorted(arr, arr.Length, 4);
+            Console.WriteLine("Linear Search UnSorted Output {0}", linearSearchUnsortedOutput);
+            int[] sortedarray = { 23, 50, 67, 89, 888, 8888 };
+            bool linearSearchsortedOutput = pobj.linearSearchSorted(sortedarray, sortedarray.Length, 8888);
+            Console.WriteLine("Linear Search Sorted Output {0}",linearSearchsortedOutput);
+            Console.ReadLine();
+        }
+
+        public  bool linearSearchUnsorted(int[] arr,int size,int value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if(value == arr[i])
+                {
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+
+        public bool linearSearchSorted(int[] arr,int size,int value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if(value == arr[i])
+                {
+                    return true;
+                }
+                else if(value < arr[i])
+                {
+                    return false;
+                }
+
+            }
+            return false;
+        }
+    }
+}
